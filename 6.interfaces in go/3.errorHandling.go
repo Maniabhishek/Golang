4e28ea41d83aa2044c141bb465d11a94cdacd037
maniabhishek <maniabhishek05@gmail.com>
2023-02-01 08:04:16 +0530

    error handling start

---
+++ 6.interfaces in go/3.errorHandling.go
@@ -0,0 +1,15 @@
+/*
+Go has no exceptions
+Errors are returned as the last return value from a function
+ encodes failure as part of the function signature
+  simple to determine if a function can fail
+ Return nil if no error occurred
+Errors implement the error interface from std
+ one function to implement: Error() string
+*/
+
+package main
+
+func errorHandling() {
+
+}
